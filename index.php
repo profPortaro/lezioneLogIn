ffc4918a19ec456c2bb5449cb676da9d0a54789c
Basilio Emiliano Portaro <45729897+profPortaro@users.noreply.github.com>
2018-12-09 10:07:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log In</title>
+</head>
+<body>
+<?php
+include_once "script//setup.php";
+//$_SESSION['prova'] = "ciaiiio";
+?>
+<form action="profilo.php" method="POST" target="_blank">
+<input type="text" name="mail" id="mail">
+<input type="password" name="psw" id="psw">
+<input type="submit" name="invia" id="">
+</form>
+
+
+
+</body>
+</html>
+
+
---
+++ profilo.php
@@ -0,0 +1,21 @@
+﻿<?php
+    include_once "script//setup.php";
+    if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
+        $mail = $_POST['mail'];
+        $psw = $_POST['psw'];
+        $db = new db("amministrazione.ini");
+        if( !$db->get_connessione() ) {
+            //gestione dell'errore
+            echo "Connessione al server fallita. Impossibile procedere. Contattare ...";
+            die;  
+        } else {
+            echo "BENVENUTO";
+        }
+
+
+    } else {
+        header("Location: index.php?errore=4"); //non autenticato
+        exit;  
+    }
+
+?>
